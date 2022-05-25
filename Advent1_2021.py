52b76efb14bbb9bf604cd3aa5b7d9891de6f67c0
Will <Wotty@users.noreply.github.com>
2022-05-25 14:30:57 +0100

    update

--- Advent1_2021.py
+++ Advent1_2021.py
@@ -8,10 +8,39 @@ def advent1part1():
         if previous < int(i):
             count += 1
         previous = int(i)
-    print(count)    
+    print(count)
+
 
 def advent1part2():
     f = open("input-2021.txt", "r")
+    contents = f.readlines()
+    list = [0] * 3
+    previous_total = 0
+    total = 0
+    index = 0
+    count = 0
+
+    for i in contents:
+        if index > 2:
+            index = 0
+
+            total = sum_of_list(list)
+            if previous_total < total:
+                count += 1
+            print(total, previous_total, list, count)
+            previous_total = total
+        else:
+            list[index] = int(i)
+            index += 1
+    print(count)
 
 
-advent1part1()
+def sum_of_list(l):
+    total = 0
+    for val in l:
+        total = total + val
+    return total
+
+
+# advent1part1()
+advent1part2()
